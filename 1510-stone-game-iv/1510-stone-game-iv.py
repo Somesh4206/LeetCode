142ff9ab62958c1de9505bccf68f11cd219eb5d2
Somesh M <someshm7662@gmail.com>
2026-08-10 21:44:22 +0530

    Time: 79 ms (100%), Space: 15.8 MB (68.75%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def winnerSquareGame(self, n):
+        dp = [False] * (n + 1)
+
+        for i in range(n + 1):
+            if not dp[i]:
+                j = 1
+
+                while i + j * j <= n:
+                    dp[i + j * j] = True
+                    j += 1
+
+                if dp[n]:
+                    return True
+
+        return False
