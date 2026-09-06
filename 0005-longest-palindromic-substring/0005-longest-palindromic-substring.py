2fdc41086b37464b56e248bbc1fb3cc8334899c9
Somesh M <someshm7662@gmail.com>
2026-09-06 12:15:18 +0530

    Time: 335 ms (92.19%), Space: 12.6 MB (19.44%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        if not s:
+            return ""
+
+        start, end = 0, 0
+
+        for i in range(len(s)):
+            len1 = self.expandAroundCenter(s, i, i)   # Odd-length palindrome
+            len2 = self.expandAroundCenter(s, i, i+1) # Even-length palindrome
+            max_len = max(len1, len2)
+
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+
+        return s[start:end+1]
+
+    def expandAroundCenter(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1  # Length of the palindrome
+
+# Example usage
+sol = Solution()
+print(sol.longestPalindrome("babad"))  # Output: "bab" or "aba"
+print(sol.longestPalindrome("cbbd"))   # Output: "bb"
